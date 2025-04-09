2f2a770ca7602a7228cdcc2887379c17091e3e91
Emo-Debian <emo@Debian.home>
2025-04-09 21:36:45 +0100

    Update all

---
+++ Degoogled.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mapfile -t lines < google_packages.txt
+
+for line in "${lines[@]}"; do
+	if adb shell pm list packages | grep "package:$line"; then
+    		echo "Delete $line"
+    		adb shell pm uninstall --user 0 "$line"
+    		sleep 2
+    	else
+    		echo "Package not found $line"
+    	fi
+done
